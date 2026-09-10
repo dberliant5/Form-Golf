637da76a71d434a317cb8f789add174b004e76db
dberliant5 <dberliant5@gmail.com>
2026-09-10 13:41:34 -0500

    test(driver): pinpoint wrapper syntax failures with node check

--- tests/check-driver-wrapper-syntax-v244.mjs
+++ tests/check-driver-wrapper-syntax-v244.mjs
@@ -1,4 +1,5 @@
 import fs from 'node:fs';
+import { spawnSync } from 'node:child_process';
 
 const wrapper=fs.readFileSync('static-test-v995.html','utf8');
 const index=fs.readFileSync('index.html','utf8');
@@ -7,9 +8,8 @@ for(const src of wrapper.matchAll(/['"](assets\/[^'"]+\.js)['"]/g)) files.add(sr
 for(const src of index.matchAll(/src=["'](assets\/[^"'?]+\.js)/g)) files.add(src[1]);
 let bad=[];
 for(const file of [...files]){
-  const code=fs.readFileSync(file,'utf8');
-  try{ new Function(code); }
-  catch(e){ bad.push({file,error:e.message}); }
+  const p=spawnSync(process.execPath,['--check',file],{encoding:'utf8'});
+  if(p.status!==0) bad.push({file,status:p.status,stderr:p.stderr,stdout:p.stdout});
 }
 console.log(JSON.stringify({checked:files.size,bad},null,2));
 if(bad.length) process.exit(1);
